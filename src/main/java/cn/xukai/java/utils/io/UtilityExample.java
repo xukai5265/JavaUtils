d828b1020d3bac13fd7d0fee7754c679c6c085d4
xukai <kxu@ling-ban.com>
2016-12-29 10:18:16 +0800

    commons-io FileUtils操作     1.创建一个文件对象     2.获取文件内容     3.判断一个目录中是否有这个文件。

--- src/main/java/cn/xukai/java/utils/io/UtilityExample.java
+++ src/main/java/cn/xukai/java/utils/io/UtilityExample.java
@@ -1,7 +1,10 @@
 package cn.xukai.java.utils.io;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.LineIterator;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -10,7 +13,10 @@ import java.io.IOException;
  */
 public class UtilityExample {
     private static final String EXAMPLE_TXT_PATH =
-            "D:\\abc\\2016-12-15\\bb2d7aa3-bdcf-4c07-82c0-66331761adac.wav";
+            "D:\\abc\\2016-12-15\\test.txt";
+
+    private static final String PARENT_DIR =
+            "D:\\abc\\2016-12-16";
     public static void runExample() throws IOException {
         //FilenameUtils
         System.out.println("Utility Classes example...");
@@ -18,6 +24,21 @@ public class UtilityExample {
         System.out.println("文件名："+FilenameUtils.getName(EXAMPLE_TXT_PATH));
         System.out.println("文件扩展名: " + FilenameUtils.getExtension(EXAMPLE_TXT_PATH));
         System.out.println("Base name of exampleTxt: " + FilenameUtils.getBaseName(EXAMPLE_TXT_PATH));
+
+        //FileUtils
+        //我们可以用 FileUtils.getFile(String) 创建一个新文件对象
+        File exampleFile = FileUtils.getFile(EXAMPLE_TXT_PATH);
+        LineIterator iter = FileUtils.lineIterator(exampleFile,"gbk");
+        System.out.println("文件内容...");
+        while (iter.hasNext()) {
+            System.out.println("\t" + iter.next());
+        }
+        iter.close();
+
+
+        // 判断一个文件是否在某个目录中.
+        File parent = FileUtils.getFile(PARENT_DIR);
+        System.out.println("Parent directory contains exampleTxt file: " + FileUtils.directoryContains(parent, exampleFile));
     }
 
     public static void main(String[] args) throws IOException {
