2fa35e68bd79bc3ad8341a502769e1b286248382
xukai <kxu@ling-ban.com>
2017-01-03 16:38:38 +0800

    时间工具类：       格式化时间戳

---
+++ src/main/java/cn/xukai/java/utils/DateUtil.java
@@ -0,0 +1,44 @@
+package cn.xukai.java.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by xukai on 2017/1/3.
+ */
+public class DateUtil {
+    // 时间戳 ------------------------------------------------------------
+    /**
+     * 时间戳转换成日期格式字符串
+     *
+     * @param seconds 精确到秒的字符串
+     * @param format
+     * @return
+     */
+    public static String timeStamp2Date(String seconds, String format) {
+        if (seconds == null || seconds.isEmpty() || seconds.equals("null")) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+    }
+
+    /**
+     * 日期格式字符串转换成时间戳
+     * @param date_str  字符串日期
+     * @param format    如：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String date2TimeStamp(String date_str, String format) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            return String.valueOf(sdf.parse(date_str).getTime() / 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
