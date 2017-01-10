ea1eaec709e79b61b7cff0baddab17231bd93e44
xukai <kxu@ling-ban.com>
2017-01-10 10:16:04 +0800

    毫秒格式化

--- src/main/java/cn/xukai/java/utils/DateUtil.java
+++ src/main/java/cn/xukai/java/utils/DateUtil.java
@@ -7,6 +7,41 @@ import java.util.Date;
  * Created by xukai on 2017/1/3.
  */
 public class DateUtil {
+    /*
+     * 毫秒转化 时-分-秒-毫秒
+     */
+    public static String formatTime(Long ms) {
+        Integer ss = 1000;
+        Integer mi = ss * 60;
+        Integer hh = mi * 60;
+        Integer dd = hh * 24;
+
+        Long day = ms / dd;
+        Long hour = (ms - day * dd) / hh;
+        Long minute = (ms - day * dd - hour * hh) / mi;
+        Long second = (ms - day * dd - hour * hh - minute * mi) / ss;
+
+        StringBuffer sb = new StringBuffer();
+        if(hour >= 0) {
+            if(hour<=9)
+                sb.append("0"+hour+":");
+            else
+                sb.append(hour+":");
+        }
+        if(minute >= 0) {
+            if(minute<=9)
+                sb.append("0"+minute+":");
+            else
+                sb.append(minute+":");
+        }
+        if(second >= 0) {
+            if (second<=9)
+                sb.append("0"+second);
+            else
+                sb.append(second);
+        }
+        return sb.toString();
+    }
     // 时间戳 ------------------------------------------------------------
     /**
      * 时间戳转换成日期格式字符串
