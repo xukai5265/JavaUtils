3fb0e4468271e6a71bb056f48a8fa6ebe983d585
xukai <kxu@ling-ban.com>
2017-01-10 10:18:42 +0800

    测试类：             DateUtil

---
+++ src/test/java/cn/xukai/java/utils/DateUtilTest.java
@@ -0,0 +1,13 @@
+package cn.xukai.java.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by xukai on 2017/1/10.
+ */
+public class DateUtilTest {
+    @Test
+    public void timeStamp2DateTest(){
+        System.out.println(DateUtil.timeStamp2Date("50", null));
+    }
+}
