9d25df255338e6974c0378372b5324d0c657cc1c
xukai <kxu@ling-ban.com>
2017-02-09 10:12:51 +0800

    StringUtils.indexOf() 用法

--- src/test/java/cn/xukai/java/utils/StringUtilsTest.java
+++ src/test/java/cn/xukai/java/utils/StringUtilsTest.java
@@ -8,6 +8,15 @@ import org.junit.Test;
  */
 public class StringUtilsTest {
     /**
+     * 返回字符串world 在字符串 helloworld 中第一次出现的位置。
+     * 如果 searchChar 没有在 str 中出现则返回-1，
+     * 如果 str 为 null 或 "" ，则也返回-1
+     */
+    @Test
+    public void indexOfTest(){
+        System.out.println(StringUtils.indexOf("helloworld", "world"));
+    }
+    /**
      * StringUtils.isEmpty() & StringUtils.isBlank() 方法的却别
      * blank 会对字符串中的空格进行判断，而empty认为空格、制表符等也是字符串的一部分
      */
