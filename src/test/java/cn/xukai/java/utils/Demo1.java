ced533eb7f1f859f6402d77bc8b45877d2331b9b
xukai <kxu@ling-ban.com>
2017-05-02 17:00:14 +0800

    for 循环的一种写法。

---
+++ src/test/java/cn/xukai/java/utils/Demo1.java
@@ -0,0 +1,14 @@
+package cn.xukai.java.utils;
+
+/**
+ * Created by xukai on 2017/5/2.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        for (boolean isLink = true; isLink;) {
+            System.out.println(isLink);
+            isLink = false;
+            System.out.println(isLink);
+        }
+    }
+}
